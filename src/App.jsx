01119b8c2b9ed0c345deaba334a5b237c3cc4d10
Utkarsh Singh <utkarshsingh30112002@gmail.com>
2024-10-20 15:28:45 +0530

    react-job-portal complete

--- src/App.jsx
+++ src/App.jsx
@@ -10,22 +10,57 @@ import HomePage from './pages/HomePage.jsx'
 import JobsPage from './pages/JobsPage.jsx'
 import NotFoundPage from './pages/NotFoundPage.jsx'
 import JobPage,{jobLoader} from './pages/JobPage.jsx'
+import AddJobPage from './pages/AddJobPage.jsx'
+import EditJobPage from './pages/EditJobPage.jsx'
 
 
-
-const router=createBrowserRouter(
-  createRoutesFromElements(
-  <Route path='/' element={<MainLayout />}>
-
-    <Route index element={<HomePage />}></Route>
-    <Route path='/jobs' element={<JobsPage />}></Route>
-    <Route path='/job/:id' element={<JobPage />} loader={jobLoader}></Route>
-    <Route path='*' element={<NotFoundPage />}></Route>
-  </Route>
-)
-);
 
 const App = () => {
+  //add job
+  async function addJob(newJob){
+    const res=await fetch('/api/jobs',{
+      method:'POST',
+      headers:{
+        'Content-Type':'application/json'
+      },
+      body:JSON.stringify(newJob)
+    });
+    return;
+  }
+
+  //deleteJob
+
+  async function deleteJob(jobId){
+    const res=await fetch(`/api/jobs/${jobId}`,{
+      method:'DELETE'
+    })
+  }
+  
+  //updateJob
+  async function updateJob(job){
+    const res=await fetch(`/api/jobs/${job.id}`,{
+      method:'PUT',
+      headers:{
+        'Content-Type':'application/json'
+      },
+      body:JSON.stringify(job)
+    });
+    return;
+  }
+  const router=createBrowserRouter(
+    createRoutesFromElements(
+    <Route path='/' element={<MainLayout />}>
+  
+      <Route index element={<HomePage />}></Route>
+      <Route path='/jobs' element={<JobsPage />}></Route>
+      <Route path='/add-job' element={<AddJobPage onSubmitForm={addJob}/>}></Route>
+      <Route path='/job/:id' element={<JobPage deleteJob={deleteJob}/>} loader={jobLoader} ></Route>
+      <Route path='/edit-job/:id' element={<EditJobPage updatedJobSubmit={updateJob}/>} loader={jobLoader} ></Route>
+      <Route path='*' element={<NotFoundPage />}></Route>
+    </Route>
+  )
+  );
+
   return <RouterProvider router={router}/>
 }
 
--- src/components/JobListing.jsx
+++ src/components/JobListing.jsx
@@ -22,16 +22,16 @@ const JobListing = ({isHome}) => {
         fetchData();
     },[])
   return (
-    <section class="bg-blue-50 px-4 py-10">
-      <div class="container-xl lg:container m-auto">
-        <h2 class="text-3xl font-bold text-indigo-500 mb-6 text-center">
+    <section className="bg-blue-50 px-4 py-10">
+      <div className="container-xl lg:container m-auto">
+        <h2 className="text-3xl font-bold text-indigo-500 mb-6 text-center">
           {isHome?'Recent Jobs':'Browse Jobs'}
         </h2>
             {isloading
               ?(<Spinner isLoading={isloading}/>)
               :(
-                <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
-                  <>{jobs.map((job)=>(<SingleJobListing job={job}/>))} </>
+                <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+                  <>{jobs.map((job)=>(<SingleJobListing key={job.id} job={job}/>))} </>
                 </div>
               )
             }
